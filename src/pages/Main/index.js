03ca4bc7fcfd97f59abeaee074bee85a18e83878
Otávio Landim <otavio.landim@gmail.com>
2019-08-28 20:17:06 -0300

    Organizando os imports e alterando a listagem dos repositorios

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
-import { Container, Form, SubmitButton, List } from './styles';
+import { Container } from '../../components/Container/index';
+import { Form, SubmitButton, List } from './styles';
 
 export default class Main extends Component {
     state = {
@@ -78,10 +80,16 @@ export default class Main extends Component {
                 </Form>
 
                 <List>
-                    {repositories.map(repo => (
-                        <li key={repo.name}>
-                            <span>{repo.name}</span>
-                            <a href="">Detalhes</a>
+                    {repositories.map(repository => (
+                        <li key={repository.name}>
+                            <span>{repository.name}</span>
+                            <Link
+                                to={`/repository/${encodeURIComponent(
+                                    repository.name
+                                )}`}
+                            >
+                                Detalhes
+                            </Link>
                         </li>
                     ))}
                 </List>
